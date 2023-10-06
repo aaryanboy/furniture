a239d8e013ccba083e426db2e10020ec659325c9
aaryanboy <aaryanboy12@gmail.com>
2023-10-06 16:14:57 +0545

    roman to intiger

--- Soln13.java
+++ Soln13.java
@@ -1,104 +1,56 @@
 public class Soln13 {
     public static void main(String[] args) {
-     String a="MCMXCIV";
+     String a="III";
      int b=a.length();
-     int d=0;
+     int[] d=new int[b];
      int el=0;
-      char c=a.charAt(0);
-      if (c=='I') {
-            d=1;
+      
+      for (int i = 0; i < b; i++) {
+        char c=a.charAt(i);
+        if (c=='I') {
+            d[i]=1;
             
         }
          else if (c =='V') {
-            d=5;
+            d[i]=5;
         }
          else if (c =='X') {
-            d=10;
+            d[i]=10;
         }
          else if (c =='L') {
-            d=50;
+            d[i]=50;
         }
          else if (c =='C') {
-            d=100;
+            d[i]=100;
         }
          else if ( c=='D') {
-            d=500;
+            d[i]=500;
         }
           else if (c =='M') {
-            d=1000;
+            d[i]=1000;
         }
-     for (int i = 0; i < b; i++) {
-       c=a.charAt(i);
-       char e=a.charAt(i+1);
+     
+      }
+      el=d[b-1];
+     System.out.println(el);
+      
+      for (int i = b-1; i > 0; i--) {
+        if (d[i] > d[i - 1]) {
+            System.out.println(el +"-"+d[i-1]);
+            el =el - d[i-1];
+            
+        } else {
+              System.out.println(el +"+"+d[i-1]);
+            el += d[i-1];
+            
+        }
+    }
+
+System.out.println(el);
 
 
 
-       if (c=='I') {
-             el=1;
-             if (c>e) {
-                d=d-el;
-                
-            }else{
-                d=d+el;
-        }}
-         else if (c =='V') {
-            el =5;
-            if (c>e) {
-                d=d-el;
-                
-            }else{
-                d=d+el;
-        }
-        }
-         else if (c =='X') {
-            el=10;
-            if (c>e) {
-                d=d-el;
-                
-            }else{
-                d=d+el;
-        }
-        }
-         else if (c =='L') {
-            el=50;
-            if (c>e) {
-                d=d-el;
-                
-            }else{
-                d=d+el;
-        }
-        }
-         else if (c =='C') {
-            el=100;
-            if (c>e) {
-                d=d-el;
-                
-            }else{
-                d=d+el;
-        }
-        }
-         else if ( c=='D') {
-            el=500;
-            if (c>e) {
-                d=d-el;
-                
-            }else{
-                d=d+el;
-        }
-        }
-          else if (c =='M') {
-            el=1000;
-            if (c>e) {
-                d=d-el;
-                
-            }else{
-                d=d+el;
-        }
-        }
-     }
-
-    System.out.println(d);
-
+    
     }
     
 }
